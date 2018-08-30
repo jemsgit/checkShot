791932ac34345e0cd3bb2deed0f440709569c9aa
Evgenii J <jemguns@gmail.com>
2018-08-30 22:32:19 +0400

    main script

---
+++ main.py
@@ -0,0 +1,46 @@
+import cv2
+import os
+import RPi.GPIO as GPIO
+from PIL import Image, ImageFilter
+from numpy import *
+from random import random
+import filters.dither as dither
+import filters.halftone as halftone
+import filters.pixelate as pixelate
+import filters.threshold as threshold
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(10, GPIO.IN)
+
+# initialize the camera
+req = os.popen("ls -l /dev/video*")
+res = req.read()
+pos = res.find("/dev/video");
+index = res[pos+10:pos+12]
+port = int(index)
+cam = cv2.VideoCapture(port)
+
+def shot_callback(ch):
+    print "Button"
+    ret, image = cam.read()
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    cv2.imshow("cartoon", gray)
+    img1 = dither.process_image(image)
+    img2 = dither.process_image(halftone)
+    img3 = dither.process_image(pixelate)
+    img4 = dither.process_image(threshold)
+    cv2.imshow("cartoon1", img1)
+    cv2.imshow("cartoon2", img2)
+    cv2.imshow("cartoon3", img3)
+    cv2.imshow("cartoon4", img4)
+
+GPIO.add_event_detect(10, GPIO.FALLING, callback=shot_callback)
+
+while True:
+    ret, image = cam.read()
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    cv2.imshow('SnapshotTest',image)
+    key = cv2.waitKey(1)
+    if key == ord('q'):
+        break
+cam.release()
